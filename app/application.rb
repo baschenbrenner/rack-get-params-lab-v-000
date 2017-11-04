052f397f1299af4832f24e37012fd5f074e92ff5
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-04 15:57:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -19,6 +19,17 @@ class Application
       resp.write "Path Not Found"
     end
 
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+    elsif req.path.match(/search/)
+      search_term = req.params["p"]
+      resp.write handle_search(search_term)
+    else
+      resp.write "That item is not found"
+    end
+
+
     resp.finish
   end
 
